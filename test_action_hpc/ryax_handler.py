4dd503bdcf3bf05d0fb90d9164d3315cf289a7c6
labi0010 <alexis.labille@etudiant.univ-reims.fr>
2024-06-12 10:18:45 +0200

    daaaa

--- test_action_hpc/ryax_handler.py
+++ test_action_hpc/ryax_handler.py
@@ -3,13 +3,15 @@
 import subprocess
  
 def handle(mod_in):
-    print(mod_in.get("sshname")+"@"+mod_in.get("ip"))
+    
     ssh = subprocess.Popen("/bin/ssh",mod_in.get("sshname")+"@"+mod_in.get("ip"), 
                     stdout=subprocess.PIPE, 
                     stderr=subprocess.PIPE,
                     capture_output=True)
     ssh.communicate("./"+mod_in.get("exec"))
     ssh.wait()
+
+    return({"exec":mod_in.get("sshname")+"@"+mod_in.get("ip")})
 
  
 
