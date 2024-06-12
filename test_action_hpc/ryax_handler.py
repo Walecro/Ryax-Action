075d37274302ca844dc6a10759fb3db178a63220
labi0010 <alexis.labille@etudiant.univ-reims.fr>
2024-06-12 15:46:53 +0200

    u

--- test_action_hpc/ryax_handler.py
+++ test_action_hpc/ryax_handler.py
@@ -7,7 +7,7 @@ def handle(mod_in):
     # Détails de connexion SSH
     hostname = mod_in.get("ip")
     username = mod_in.get("sshname")
-
+    err = ""
     # Fichier à exécuter et fichier à lire
     remote_exe_path = mod_in.get("exec")
     output_file_path = "/home/alabille/"+mod_in.get("outname") 
@@ -19,9 +19,8 @@ def handle(mod_in):
         stdout, stderr = process_execute.communicate()
 
         if process_execute.returncode != 0:
-            print(f"Erreur lors de l'exécution du .exe : {stderr.decode().strip()}")
-        else:
-            print("Exécution du .exe réussie.")
+            err = (f"Erreur lors de l'exécution du .exe : {stderr.decode().strip()}")
+
 
         # Commande pour récupérer le contenu du fichier de sortie
         ssh_command_scp = f"scp {username}@{hostname}:{output_file_path} {username}@10.22.3.99:{output_file_path}"
@@ -29,15 +28,12 @@ def handle(mod_in):
         stdout, stderr = process_scp.communicate()
 
         if process_scp.returncode != 0:
-            print(f"Erreur lors de la lecture du fichier de sortie : {stderr.decode().strip()}")
-        else:
-            print("Contenu du fichier de sortie :")
-            print(open("./test.txt","r").read())
+            err = (f"Erreur lors de la lecture du fichier de sortie : {stderr.decode().strip()}")
 
     except Exception as e:
-        print(f"Erreur : {e}")
+        err = (f"Erreur : {e}")
 
-    return ({"output":open("/home/alabille/test.txt","r")})
+    return ({"output":open("/home/alabille/test.txt","r"),"err":err})
 
 
 
