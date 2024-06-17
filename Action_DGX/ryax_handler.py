d585a64393d1fe5a2b27c3eb26f103c51818cbf3
labi0010 <alexis.labille@etudiant.univ-reims.fr>
2024-06-17 11:15:13 +0200

    DGX_SSH

--- Action_DGX/ryax_handler.py
+++ Action_DGX/ryax_handler.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python3
 
-import subprocess
+import paramiko
  
-def handle(mod_in):
+def handle(input_values : dict) -> None:
     
-    # Détails de connexion SSH
-    hostname = mod_in.get("ip")
-    username = mod_in.get("sshname")
-    err = ""
-    out = ""
-    # Fichier à exécuter et fichier à lire
-    remote_exe_path = mod_in.get("exec")
+    cmd=input_values.get("ssh_cmd")
 
-    try:
-        # Commande pour se connecter en SSH et exécuter le .exe
-        ssh_command_execute = f"ssh  {username}@{hostname} '/home/alabille/a.out'"
-        process_execute = subprocess.Popen(ssh_command_execute, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process_execute.communicate()
+    print("Creating ssh key from file...")
+    pkey = paramiko.RSAKey.from_private_key_file(input_values.get("ssh_pkey"))
 
-        if process_execute.returncode != 0:
-            err = (f"Erreur lors de l'exécution du .exe : {stderr.decode().strip()}")
-        else:
-            out = "ok"
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        input_values.get("ssh_host"),
+        input_values.get("ssh_port"),
+        username=input_values.get("ssh_user"),
+        pkey=pkey,
+    )
+    print(f"Executing command '{cmd}'")
+    stdin, stdout, stderr = client.exec_command(cmd)
 
-    except Exception as e:
-        err = (f"Erreur : {e}")
+    stdout_output=""
+    stdout = stdout.readlines() if stdout else []
+    for line in stdout:
+        stdout_output += line
+    if stdout_output != "":
+        print("STDOUT===> "+stdout_output)
 
-    return ({"err":err})
+    stderr_output=""
+    stderr = stderr.readlines() if stderr else []
+    for line in stderr:
+        stderr_output += line
+    if stderr_output != "":
+        print("STDERR===> "+stderr_output)
+
+    client.close()
+
+    return { "stdout": str(stdout_output), "stderr": str(stderr_output)}
+
 
 
 
