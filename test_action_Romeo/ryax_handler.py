0964b89ac8f0b8e237c2b246bc98930d38424c67
labi0010 <alexis.labille@etudiant.univ-reims.fr>
2024-07-01 11:43:56 +0200

    fix_dependencies

--- test_action_Romeo/ryax_handler.py
+++ test_action_Romeo/ryax_handler.py
@@ -6,14 +6,14 @@ def handle(mod_in):
     
     pkey = paramiko.RSAKey.from_private_key_file(mod_in.get("ssh_pkey"))
     err = ""
-    #client = paramiko.SSHClient()
-    #client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #client.connect(
-    #    mod_in.get("ssh_host"),
-    #    mod_in.get("ssh_port"),
-    #    username=mod_in.get("ssh_user"),
-    #    pkey=pkey,
-    #)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        mod_in.get("ssh_host"),
+        mod_in.get("ssh_port"),
+        username=mod_in.get("ssh_user"),
+        pkey=pkey,
+    )
     
     ###
     # stdin, stdout, stderr = client.exec_command('echo "#!/bin/bash\n#SBATCH --time=00:01:00\n#SBATCH --nodes=2 \nsrun ls -a" >batch.sh')
@@ -21,7 +21,7 @@ def handle(mod_in):
        # err += line
 
     #stdin, stdout, stderr = client.exec_command('sbatch batch.sh > output.out')
-    #client.close()
+    client.close()
 
     #for line in stderr.readlines():
         #err += line
