ed4b765f3b5f65089ddd757c8bbcce9a5b174469
labi0010 <alexis.labille@etudiant.univ-reims.fr>
2024-07-11 20:05:37 +0200

    final?

--- Offload_Hpc/ryax_handler.py
+++ Offload_Hpc/ryax_handler.py
@@ -84,6 +84,7 @@ def handle(mod_in):
         )
         rb_stdin, rb_stdout, rb_stderr = client.exec_command(cmdsbatchromeo)
         rex_stdin, rex_stdout, rex_stderr = client.exec_command(cmdexecromeo)
+        client.close()
     elif(res_avail_dgx >= mod_in.get("nodes") and mod_in.get("resource") == "gpu"):
         #Exec sur DGX
         client.connect(
@@ -93,12 +94,12 @@ def handle(mod_in):
             pkey=pkey,
         )
         client.exec_command(cmdexecdgx)
+        client.close()
     else:
         ret = "Pas de ressource"
 
-    client.close()
 
 
     err = r_stderr.readlines() + d_stderr.readlines() + rb_stderr.readlines() + rex_stderr.readlines()
 
-    return({"err":"osef","res_DEBUG":ret})
+    return({"err":"osef","res_DEBUG":err})
